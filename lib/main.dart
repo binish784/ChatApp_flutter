2285df052e22c8021dc8d18c5ca468984dd47c98
binish784 <binish784@gmail.com>
2019-03-18 00:14:22 +0545

    Complete program overHaul

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './message.dart';
 
 void main(){
   return runApp(new MainApp());
@@ -40,7 +40,7 @@ class MainApp extends StatelessWidget {
   }
 
   Widget showMessage(Text name, Text message, Image source){
-    return Container(
+    return new Container(
               margin: EdgeInsets.only(left: 5,top:5),
               height: 70,
               child:ListTile( 
@@ -56,17 +56,19 @@ class MainApp extends StatelessWidget {
     );
   }
 
+
   Widget build(BuildContext context) {
+   
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: new Scaffold(
         appBar: AppBar(
           title: createIconRow()
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              TextField(
+        body:
+        Column(
+          children: <Widget>[
+            TextField(
                 style: TextStyle(color: Colors.indigo),
                 decoration: InputDecoration(
                   contentPadding: EdgeInsets.only(left: 40,top:15),
@@ -74,17 +76,15 @@ class MainApp extends StatelessWidget {
                   hintText: "Search...",
                 ),
               ),
-              showMessage(Text("Binish Manandhar"), Text("THis was great"),Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVTCmTaeDm1FyOLRQ7stWHYR3WzE_5ViREIQJOMRkGm21wb8l-")),
-              showMessage(Text("Andrew Shaman"), Text("Thanks to Flutter Nepal"),Image.network("https://upload.wikimedia.org/wikipedia/commons/3/38/Wikipedia_User-ICON_byNightsight.png")),
-              showMessage(Text("Nepal Meow"), Text("Great Opportunity to learn Flutter"),Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5y3L0URosUA7io9O_b5YmaCqC80dokrs5NimYJ8xZ8o7UploB")),
-              showMessage(Text("Howdy dowdy"), Text("Nice workshop and community"),Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaFfIZjtmUjzI94KOcU50tSXMguL8gnWvQZiOZXBLpqvAhMJ5K")),
-              showMessage(Text("Lee"), Text("First Flutter Project"),Image.network("https://cdn.pixabay.com/photo/2016/04/15/18/05/computer-1331579_960_720.png")),
-              showMessage(Text("Jessi"), Text("Second Assignment Flutter Nepal"),Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaFfIZjtmUjzI94KOcU50tSXMguL8gnWvQZiOZXBLpqvAhMJ5K")),
-              showMessage(Text("Brenden"), Text("Didn't do the first one though"),Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5y3L0URosUA7io9O_b5YmaCqC80dokrs5NimYJ8xZ8o7UploB")),
-              showMessage(Text("Andrea"), Text("Well No problem :p"),Image.network("https://cdn.pixabay.com/photo/2016/04/15/18/05/computer-1331579_960_720.png")),
-              
-            ],
-          ),
+            Expanded(
+              child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (BuildContext context, int index){
+                  return showMessage(Text(messages[index]["username"]), Text(messages[index]["message"]),Image.network(messages[index]["image"]));
+                },
+              ),
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
---
+++ lib/message.dart
@@ -0,0 +1,43 @@
+
+ List messages=[
+      {
+        'username':"Binish Manandhar",
+        'message':"This was great",
+        'image':"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVTCmTaeDm1FyOLRQ7stWHYR3WzE_5ViREIQJOMRkGm21wb8l-",
+      },
+      {
+        'username':"Andrew Shaman",
+        'message':'Thanks to Flutter Nepal',
+        'image':'https://upload.wikimedia.org/wikipedia/commons/3/38/Wikipedia_User-ICON_byNightsight.png',
+      },
+      {
+        'username':'Nepal Meow',
+        'message':'Great Opportunity to learn Flutter',
+        'image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5y3L0URosUA7io9O_b5YmaCqC80dokrs5NimYJ8xZ8o7UploB',
+      },
+      {
+        'username':'Howdy dowdy',
+        'message':'Nice workshop and community',
+        'image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaFfIZjtmUjzI94KOcU50tSXMguL8gnWvQZiOZXBLpqvAhMJ5K',
+      },
+      {
+        'username':'Lee',
+        'message':'First Flutter Project : Messenger Assignment',
+        'image':'https://cdn.pixabay.com/photo/2016/04/15/18/05/computer-1331579_960_720.png',
+      },
+      {
+        'username':'Jessica',
+        'message':'Second Assignment Flutter Nepal',
+        'image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaFfIZjtmUjzI94KOcU50tSXMguL8gnWvQZiOZXBLpqvAhMJ5K',
+      },
+      {
+        'username':'Brenden',
+        'message':'Didnt do the first one though',
+        'image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5y3L0URosUA7io9O_b5YmaCqC80dokrs5NimYJ8xZ8o7UploB',
+      },
+      {
+        'username':'Andrea',
+        'message':'Well No problem :p',
+        'image':'https://cdn.pixabay.com/photo/2016/04/15/18/05/computer-1331579_960_720.png',
+      },
+    ];
